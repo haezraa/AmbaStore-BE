b675e81f9665cd229255ca6140ee48c0d358af7c
haezraa <akbarhezra27@gmail.com>
2026-02-17 21:51:51 +0700

    #5

--- app/Models/Nominal.php
+++ app/Models/Nominal.php
@@ -7,6 +7,15 @@ class Nominal extends Model
 {
     protected $guarded = [];
 
+    public function getImageAttribute($value)
+    {
+        if ($value) {
+            return url('storage/' . $value);
+        }
+
+        return null;
+    }
+
     public function game() {
         return $this->belongsTo(Game::class);
     }
