abc95f800338f678668bb6899f2ba0ddec5a4643
haezraa <akbarhezra27@gmail.com>
2026-03-03 22:11:35 +0700

    #11

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\GameController;
 use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/games', [GameController::class, 'index']);
@@ -13,6 +14,13 @@ Route::get('/games/{id}', [GameController::class, 'show']);
 Route::post('/check-nickname', [\App\Http\Controllers\Api\GameController::class, 'checkNickname']);
 Route::get('/transaction/{invoice}', [TransactionController::class, 'checkStatus']);
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
